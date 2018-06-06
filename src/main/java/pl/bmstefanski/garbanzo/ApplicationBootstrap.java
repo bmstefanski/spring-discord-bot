8df8cd19ee03109e714d53eefdb76f78f49e2007
bmstefanski <bstefanskicom@gmail.com>
2018-06-06 21:41:14 +0200

    Implement spring boot

---
+++ src/main/java/pl/bmstefanski/garbanzo/ApplicationBootstrap.java
@@ -0,0 +1,13 @@
+package pl.bmstefanski.garbanzo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationBootstrap {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ApplicationBootstrap.class, args);
+  }
+
+}
