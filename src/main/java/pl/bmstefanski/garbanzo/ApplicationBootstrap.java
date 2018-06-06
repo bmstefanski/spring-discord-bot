aba8b1d8925f993d5950c795d44a292bd039b3b6
bmstefanski <bstefanskicom@gmail.com>
2018-06-06 22:58:48 +0200

    Implement internationalization, set up jda

--- src/main/java/pl/bmstefanski/garbanzo/ApplicationBootstrap.java
+++ src/main/java/pl/bmstefanski/garbanzo/ApplicationBootstrap.java
@@ -1,13 +1,34 @@
 package pl.bmstefanski.garbanzo;
 
+import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDABuilder;
+import net.dv8tion.jda.core.hooks.AnnotatedEventManager;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class ApplicationBootstrap {
+public class ApplicationBootstrap implements CommandLineRunner {
+
+  private final Environment environment;
+
+  @Autowired
+  public ApplicationBootstrap(Environment environment) {
+    this.environment = environment;
+  }
 
   public static void main(String[] args) {
     SpringApplication.run(ApplicationBootstrap.class, args);
+  }
+
+  @Override
+  public void run(String... args) throws Exception {
+    new JDABuilder(AccountType.BOT)
+        .setToken(this.environment.getProperty("jda.discord.token"))
+        .setEventManager(new AnnotatedEventManager())
+        .buildBlocking();
   }
 
 }
---
+++ src/main/java/pl/bmstefanski/garbanzo/component/MessageComponent.java
@@ -0,0 +1,29 @@
+package pl.bmstefanski.garbanzo.component;
+
+import javax.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.support.MessageSourceAccessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageComponent {
+
+  private final MessageSource messageSource;
+  private MessageSourceAccessor messageSourceAccessor;
+
+  @Autowired
+  public MessageComponent(MessageSource messageSource) {
+    this.messageSource = messageSource;
+  }
+
+  @PostConstruct
+  private void initialize() {
+    this.messageSourceAccessor = new MessageSourceAccessor(this.messageSource);
+  }
+
+  public String get(String key, String... args) {
+    return this.messageSourceAccessor.getMessage(key, args);
+  }
+
+}
