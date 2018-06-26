77e88f7e0cce6796557c0a655502fa3dbef90c47
Bartłomiej Stefański <bstefanskicom@gmail.com>
2018-06-26 23:21:50 +0200

    Commit command improvements

--- src/main/java/pl/bmstefanski/garbanzo/command/CommitCommand.java
+++ src/main/java/pl/bmstefanski/garbanzo/command/CommitCommand.java
@@ -5,18 +5,18 @@ import com.mashape.unirest.http.JsonNode;
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
 import java.awt.Color;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.MessageEmbed;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import pl.bmstefanski.garbanzo.command.defaults.CommandExecutor;
 import pl.bmstefanski.garbanzo.command.defaults.CommandInfo;
 import pl.bmstefanski.garbanzo.command.defaults.CommandSender;
 
 public class CommitCommand implements CommandExecutor {
 
-  @CommandInfo(value = "commit", minArguments = 1, maxArguments = 2, usage = "<username> [2017/Yyyy-Mm-Dd/today]")
+  @CommandInfo(value = "commit", minArguments = 1, maxArguments = 2, usage = "<username> [Yyyy-Mm-Dd]")
   @Override
   public void execute(CommandSender commandSender, List<String> args) {
     try {
@@ -26,26 +26,54 @@ public class CommitCommand implements CommandExecutor {
           .routeParam("username", args.get(0))
           .asJson();
 
-      Map<String, Integer> commitsPerYearMap = new HashMap<>();
-      JSONArray yearsArrayJson = httpResponse.getBody().getObject().getJSONArray("years");
+      JSONArray yearsJsonArray = httpResponse.getBody().getObject().getJSONArray("years");
 
-      for (int i = 0; i < yearsArrayJson.length(); i++) {
-        commitsPerYearMap.put(yearsArrayJson.getJSONObject(i).getString("year"), yearsArrayJson.getJSONObject(i).getInt("total"));
+      if (yearsJsonArray.length() == 0) {
+        MessageEmbed messageEmbeds = new EmbedBuilder()
+            .setColor(Color.RED)
+            .setFooter("Username doesn't match to any account!", null)
+            .build();
+
+        commandSender.sendEmbedMessage(messageEmbeds);
+        return;
       }
 
       if (args.size() == 1) {
         EmbedBuilder embedBuilder = new EmbedBuilder()
             .setColor(Color.GREEN)
-            .setTitle(args.get(0) + "'s" + " GitHub activity");
+            .setTitle(args.get(0) + "'s" + " activity");
 
-        commitsPerYearMap.forEach((year, commits) -> {
-          embedBuilder.addField("Year " + year + ":", String.valueOf(commits), true);
-        });
+        for (int i = 0; i < yearsJsonArray.length(); i++) {
+          embedBuilder.addField("Year "
+              + yearsJsonArray.getJSONObject(i).getString("year")
+              + ":", String.valueOf(yearsJsonArray.getJSONObject(i).getInt("total")), true);
+        }
 
         commandSender.sendEmbedMessage(embedBuilder.build());
         return;
       }
 
+      JSONArray contributionsJsonArray = httpResponse.getBody().getObject()
+          .getJSONArray("contributions");
+
+      EmbedBuilder embedBuilder = new EmbedBuilder()
+          .setColor(Color.RED)
+          .setFooter("Cannot find activity from: " + args.get(1), null);
+
+      for (int i = 0; i < contributionsJsonArray.length(); i++) {
+        JSONObject jsonObject = contributionsJsonArray.getJSONObject(i);
+
+        if (args.get(1).equals(jsonObject.getString("date"))) {
+          embedBuilder.setTitle(args.get(0) + "'s" + " activity");
+          embedBuilder.setColor(Color.decode(jsonObject.getString("color")));
+          embedBuilder.addField("Searching date", args.get(1), true);
+          embedBuilder.addField("Contributions count", String.valueOf(jsonObject.getInt("count")), true);
+          embedBuilder.setFooter(null, null);
+        }
+
+      }
+
+      commandSender.sendEmbedMessage(embedBuilder.build());
     } catch (UnirestException e) {
       e.printStackTrace();
     }
